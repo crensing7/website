b815c73b83e48071518e8a5a4d98a3c14fbeb376
Neusop <crensing7@gmail.com>
2025-07-29 10:12:39 -0500

    initial commit

---
+++ server.py
@@ -0,0 +1,10 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+    return flask.render_template('first.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
