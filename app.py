a8300c802ed98064e494e4bb5b68e4deba57855a
Neusop <crensing7@gmail.com>
2025-07-30 00:24:23 -0500

    oooh colors

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return flask.render_template('first.html')
+    return flask.render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
