ac6710c0a6ecfb726b8e40b558e2670093621c3a
Neusop <crensing7@gmail.com>
2025-08-01 01:10:18 -0500

    cert display

---
+++ static/js/script.js
@@ -0,0 +1,11 @@
+document.getElementById("proj").addEventListener("click", function() {
+        document.getElementById("carousel").className = "showProj";
+});
+
+document.getElementById("cert").addEventListener("click", function() {
+        document.getElementById("carousel").className = "showCert";
+});
+
+document.getElementById("lang").addEventListener("click", function() {
+        document.getElementById("carousel").className = "showLang";
+});
