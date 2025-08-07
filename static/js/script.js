edf7ada95291426059ef3fe4f4b71dabb7c6a737
Neusop <crensing7@gmail.com>
2025-08-06 21:57:53 -0500

    v1

--- static/js/script.js
+++ static/js/script.js
@@ -3,8 +3,10 @@ const certButton = document.getElementById('cert');
 const langButton = document.getElementById('lang');
 const carousel = document.getElementById('carousel');
 const projDiv = document.getElementById('projects');
-const certsDiv = document.getElementById('certifications');
-const langsDiv = document.getElementById('languages');
+const certDiv = document.getElementById('certifications');
+const langDiv = document.getElementById('languages');
+const github = document.getElementById('github');
+const credly = document.getElementById('credly');
 
 function setCarouselState(state) {
         carousel.classList.remove('left', 'center', 'right');
@@ -15,29 +17,49 @@ function setProjVisibility(show) {
         if (show) {
                 projDiv.classList.add('show');
                 projDiv.classList.remove('hidden');
+                projButton.parentElement.classList.remove('idle');
+                projButton.parentElement.classList.add('highlight');
+                github.classList.remove('idle');
+                github.classList.add('highlight');
         } else {
                 projDiv.classList.add('hidden');
                 projDiv.classList.remove('show');
+                projButton.parentElement.classList.remove('highlight');
+                projButton.parentElement.classList.add('idle');
+                github.classList.remove('highlight');
+                github.classList.add('idle');
         }
 }
 
 function setCertsVisibility(show) {
         if (show) {
-                certsDiv.classList.add('show');
-                certsDiv.classList.remove('hidden');
+                certDiv.classList.add('show');
+                certDiv.classList.remove('hidden');
+                certButton.parentElement.classList.remove('idle');
+                certButton.parentElement.classList.add('highlight');
+                credly.classList.remove('idle');
+                credly.classList.add('highlight');
         } else {
-                certsDiv.classList.add('hidden');
-                certsDiv.classList.remove('show');
+                certDiv.classList.add('hidden');
+                certDiv.classList.remove('show');
+                certButton.parentElement.classList.remove('highlight');
+                certButton.parentElement.classList.add('idle');
+                credly.classList.remove('highlight');
+                credly.classList.add('idle');
         }
 }
 
 function setLangsVisibility(show) {
         if (show) {
-                langsDiv.classList.add('show');
-                langsDiv.classList.remove('hidden');
+                langDiv.classList.add('show');
+                langDiv.classList.remove('hidden');
+                langButton.parentElement.classList.remove('idle');
+                langButton.parentElement.classList.add('highlight');
         } else {
-                langsDiv.classList.add('hidden');
-                langsDiv.classList.remove('show');
+                langDiv.classList.add('hidden');
+                langDiv.classList.remove('show');
+                langButton.parentElement.classList.remove('highlight');
+                langButton.parentElement.classList.add('idle');
         }
 }
 
@@ -48,6 +70,7 @@ projButton.addEventListener('click', () => {
         setCertsVisibility(false);
         setLangsVisibility(false);
 });
+
 langButton.addEventListener('click', () => {
         console.log('lang');
         setCarouselState('right');
@@ -55,6 +78,7 @@ langButton.addEventListener('click', () => {
         setCertsVisibility(false);
         setLangsVisibility(true);
 });
+
 certButton.addEventListener('click', () => {
         console.log('cert');
         setCarouselState('left');
